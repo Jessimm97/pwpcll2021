3a72e37777eb9a259964eaf911c9948dcc768b9f
Jessica <jessimm97@gmail.com>
2021-06-10 21:44:19 -0500

    Sesion 30

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-import winston from 'winston';
+import winston from '@server/config/winston';
 
 import indexRouter from '@s-routes/index';
 import usersRouter from '@s-routes/users';
@@ -56,7 +56,7 @@ if (env === 'development') {
 // view engine setup
 configTemplateEngine(app);
 
-app.use(morgan('combined',{stream: winston.stream }));
+app.use(morgan('combined',{ stream: winston.stream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -64,17 +64,33 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
+   // Log
+ winston.error(
+  `Code: 404, Message: Page Not Found, URL: ${req.originalUrl}, Method: ${req.method}`
+);
   next(createError(404));
 });
+
+
+
 // error handler
 app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+// Loggeando con Winston
+winston.error(
+  `status: ${err.status || 500}, Message: ${err.message}, Method: ${
+    req.method
+  }, IP:${req.ip}`
+);
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+
 });
 module.exports = app;
